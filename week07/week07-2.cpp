1dda193d6aae357e196628c778fcca9bd7230100
陳 <91250419+ting10160083@users.noreply.github.com>
2021-10-28 17:47:11 +0800

    Add files via upload

---
+++ week07/week07-2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    if(n>0){
+        printf("n>0³á\n");
+    }
+    while(n>0){
+        printf("n¦³¶i¨Ó³á\n");
+    }
+}
---
+++ week07/week07-3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n=3;
+    while(n>0){
+        printf("n¦³¶i¨Ó³án:%d\n",n);
+        n--;
+    }
+    printf("n:%d\n",n);
+}
+
