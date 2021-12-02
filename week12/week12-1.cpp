966ac42c25fd876fe5cef99b2e0fa1271e56d5b6
陳 <91250419+ting10160083@users.noreply.github.com>
2021-12-02 17:10:35 +0800

    Add files via upload

---
+++ week12/week12-1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int a[10]={9,8,7,6,5,4,3,2,1,0};
+int main()
+{
+    for(int i=0;i<10;i++) printf("%d ",a[i]);
+    printf("開始之前\n");
+
+    for(int i=0;i<10;i++){
+        for(int j=i+1;j<10;j++){
+            if(a[i]>a[j]){ ///比較-錯了交換
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    for(int i=0;i<10;i++) printf("%d ",a[i]);
+    printf("最後結束\n");
+}
